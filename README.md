bd2cb736e3072a0604e00f468a12061aedf03947
Eduardo Sanz García <esanzgar@gmail.com>
2019-03-28 11:55:05 +0000

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+[![CircleCI](https://circleci.com/gh/esanzgar/mythical-shop.svg?style=svg)](https://circleci.com/gh/esanzgar/mythical-shop)
+
+# Mythical shop
+
+An Angular 7 app to browse and shop mythical items.
+
+A live and editable version of the app can be accessed
+[here](https://stackblitz.com/github/esanzgar/mythical-shop)
+
+## Development server
+
+To develop locally run the development server following one of these recipes:
+
+1. With yarn:
+
+```
+yarn install [--frozen-lockfile]
+yarn start
+```
+
+2. With npm:
+
+```
+npm ci
+npm start
+```
+
+## Want to help?
+
+Want to file a bug, contribute some code, or improve documentation? Excellent!
+Read up on our guidelines for [contributing][contributing].
+
+[contributing]: https://github.com/esanzgar/mythical-shop/blob/master/CONTRIBUTING.md
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { RootComponent } from './containers/root/root.component';
+
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule
+} from '@angular/material';
+
+@NgModule({
+  declarations: [RootComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  providers: [],
+  bootstrap: [RootComponent]
+})
+export class AppModule {}
