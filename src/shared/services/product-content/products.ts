efc0e683af8175845d175e0f06c17a8a27a56e15
Eduardo Sanz García <esanzgar@gmail.com>
2019-04-24 21:05:41 +0100

    fix: husky prepare-commit-message

--- src/shared/services/product-content/products.ts
+++ src/shared/services/product-content/products.ts
@@ -2,5 +2,12 @@ const descriptions = {
   VqKb4tyj9V6i:
     "This shield is crafted from the finest gold, found deep in the center of the Krackatowa. The gold is then carefully extracted from the lava and poured staight into a mould. The double thick layer of ores in between the shield help reinforce the gold to keep its shape and strength. Gold from the Krackatowa is prized for its durability and strenght, as well as being a very beautiful ore. Gold of this kind is greatly valued by goblins who find this gold to be the purest ore around. So why not impres your friends at a dinerparty or a picnic by bring along this one of a kind shield? It's the latest model, and has been used by Tulula da second, hier of the English throne and the bravest warrior worm that has ever lived. So why not buy now, your super fashionable compleatly everything proof shield? A real bargain!",
   DXSQpv6XVeJm:
-    "This one of a kind helmet has been worn by some of the greatest warriors and knights that have ever lived, such as *Sweaty the smelly Yak and Twoey the snail, these are some of the bravets heroes of our time! The helmet is made of pure silve, probably one of the heaviest metals around,this means that wearnig the Helmet will be good exersice for your neck. So why don't you buy the super heavey super pricey Silver Helmet now? *Warning, Smelly and Twoey were very prone to headlice so please wash cearfully before use.    "
+    "This one of a kind helmet has been worn by some of the greatest warriors and knights that have ever lived, such as *Sweaty the smelly Yak and Twoey the snail, these are some of the bravets heroes of our time! The helmet is made of pure silve, probably one of the heaviest metals around,this means that wearnig the Helmet will be good exersice for your neck. So why don't you buy the super heavey super pricey Silver Helmet now? *Warning, Smelly and Twoey were very prone to headlice so please wash cearfully before use.",
+  '7dgX6XzU3Wds': 'Sword',
+  PKM5pGAh9yGm: 'Axe',
+  '7Hv0hA2nmci7': 'Knife',
+  '500R5EHvNlNB': 'Gold Coin',
+  IP3cv7TcZhQn: 'Platinum Coin',
+  IJOHGYkY2CYq: 'Bow',
+  '8anPsR2jbfNW': 'Silver Coin'
 };
